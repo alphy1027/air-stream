def407104ad81cd574e97b8563497002ebaba9fd
alphy1027 <alphy1027@gmail.com>
2025-10-12 22:09:50 +0300

    params type bug correction

--- src/app/services/[slug]/page.tsx
+++ src/app/services/[slug]/page.tsx
@@ -6,8 +6,9 @@ import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import React from "react";
 
-export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
-  const serviceInfo = services.find((service) => service.link === params.slug);
+export async function generateMetadata({ params }: { params: Promise<{ slug: string }> }): Promise<Metadata> {
+  const { slug } = await params;
+  const serviceInfo = services.find((service) => service.link === slug);
   if (!serviceInfo) {
     return notFound();
   }
@@ -17,12 +18,12 @@ export async function generateMetadata({ params }: { params: { slug: string } })
     title: `${service} | AirStream HVAC & Refrigeration Services in Kenya`,
     description: `AirStream provides expert ${service.toLowerCase()} services in Kenya, including installation, repair, maintenance, and genuine spare parts.`,
     alternates: {
-      canonical: `/services/${params.slug}`,
+      canonical: `/services/${params}`,
     },
     openGraph: {
       title: `${service} | AirStream HVAC & Refrigeration`,
       description: `Discover AirStream’s ${service.toLowerCase()} solutions in Kenya. Reliable installation, repair, maintenance, and spare parts.`,
-      url: `/services/${params.slug}`,
+      url: `/services/${params}`,
     },
   };
 }

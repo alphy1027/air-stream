e5c5754f48d4c8adf157dc09a5f2c173f34946ae
alphy1027 <alphy1027@gmail.com>
2025-10-12 22:37:44 +0300

    params type bug correction

--- src/app/services/[slug]/page.tsx
+++ src/app/services/[slug]/page.tsx
@@ -28,8 +28,8 @@ export async function generateMetadata({ params }: { params: Promise<{ slug: str
   };
 }
 
-export default function ServicePage({ params }: { params: { slug: string } }) {
-  const { slug } = params;
+export default async function ServicePage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
   const service = services.find((service) => service.link === slug);
 
   if (!service) {

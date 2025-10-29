338b8806f73eb2df8908d3f7b3354ac4bba3f934
alphy1027 <alphy1027@gmail.com>
2025-10-29 16:59:41 +0300

    adjusted product card size

--- src/app/home/components/HomeProjectCard.tsx
+++ src/app/home/components/HomeProjectCard.tsx
@@ -3,9 +3,9 @@ import { projects } from "@/content/projects";
 
 export default function HomeProjectCard({ project }: { project: (typeof projects)[0] }) {
   return (
-    <div className="border w-[180px] md:w-[230px] lg:w-[280px] border-foreground-borders gap-brand-spacing-sm flex flex-col pb-brand-spacing-sm">
-      <div className="  h-[280px] md:h-[300px] lg:h-[330px] bg-neutral rounded-t-brand-radius" />
-      <div className="flex-col flex gap-brand-spacing-xs px-brand-spacing-xs">
+    <div className="border w-[280px] rounded-brand-radius border-foreground-borders gap-brand-spacing-sm flex flex-col pb-brand-spacing-sm">
+      <div className="h-[330px] bg-neutral rounded-t-brand-radius" />
+      <div className="flex-col flex gap-brand-spacing-xs px-brand-spacing-sm">
         <ProjectDate />
         <h6 className="">{project.title}</h6>
       </div>
--- src/app/projects/components/ProjectDate.tsx
+++ src/app/projects/components/ProjectDate.tsx
@@ -1,6 +1,6 @@
 export default function ProjectDate() {
   return (
-    <small className="uppercase bg-neutral text-foreground-muted px-2 py-0.5 border border-foreground-borders rounded-brand-radius w-fit">
+    <small className="uppercase bg-neutral text-foreground-muted text-caption-sm px-1 border border-foreground-borders rounded-brand-radius w-fit">
       18 September 2025
     </small>
   );

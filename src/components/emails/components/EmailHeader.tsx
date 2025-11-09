5685b58bdcd161ab0afe99920acb6b0e4c596e64
alphy1027 <alphy1027@gmail.com>
2025-11-09 15:51:55 +0300

    adjusted email template logo size

--- src/components/emails/components/EmailHeader.tsx
+++ src/components/emails/components/EmailHeader.tsx
@@ -1,14 +1,12 @@
-import { Img, Link, Section } from "@react-email/components";
+import { Img, Link } from "@react-email/components";
 
 export const baseUrl = process.env.SITE_URL ? `https://${process.env.SITE_URL}` : "";
 
 export default function EmailHeader() {
   return (
-    <Section>
-      <Link style={logo} target="_blank" href={baseUrl}>
-        <Img src={`${baseUrl}/images/airstream-logo.png`} width="198" height="40" alt="Airstream logo" />
-      </Link>
-    </Section>
+    <Link style={logo} target="_blank" href={baseUrl}>
+      <Img src={`${baseUrl}/images/airstream-logo.png`} width="172" height="48" alt="Airstream logo" />
+    </Link>
   );
 }
 const logo = {

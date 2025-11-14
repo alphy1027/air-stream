01f7b7accc71c8285ece5eb6f7c0cfe726bea331
alphy1027 <alphy1027@gmail.com>
2025-11-14 15:13:55 +0300

    added whatsapp icon in instant contact

--- src/app/contact-us/components/InstantContact.tsx
+++ src/app/contact-us/components/InstantContact.tsx
@@ -1,23 +1,23 @@
 import Text from "@/components/UI-primitives/text/Text";
-import { LucideProps } from "lucide-react";
 import Link from "next/link";
+import { ReactNode } from "react";
 
 type ContactProps = {
   title: string;
-  icon: React.ForwardRefExoticComponent<Omit<LucideProps, "ref"> & React.RefAttributes<SVGSVGElement>>;
+  icon: ReactNode;
   body: string;
   href: string;
 };
 
-export default function InstantContact({ title, icon: Icon, href, body }: ContactProps) {
+export default function InstantContact({ title, icon, href, body }: ContactProps) {
   return (
     <Link
       href={href}
-      className="flex flex-col gap-brand-spacing-sm rounded-brand-radius bg-neutra border border-foreground-borders py-brand-spacing-sm px-brand-spacing hover:scale-105 duration-200 transition ease-in group hover:bg-accent hover:border-none"
+      className="flex flex-col gap-brand-spacing-sm rounded-brand-radius bg-neutra border border-foreground-borders py-brand-spacing-sm px-brand-spacing hover:scale-105 duration-200 transition ease-in group hover:bg-accent hover:border-none active:bg-accent active:border-none"
     >
       <h5 className="font-bold">{title}</h5>
       <div className="flex items-center gap-brand-spacing-xs">
-        <Icon />
+        {icon}
         <Text className="text-body-lg font-medium tracking-wide">{body}</Text>
       </div>
     </Link>

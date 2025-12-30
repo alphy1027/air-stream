90169ca58fd7c21f49ef300ec7df6e5dfa9a5c7c
alphy1027 <alphy1027@gmail.com>
2025-12-30 15:37:47 +0300

    added new catalogue pdf

--- src/app/contact-us/components/InstantContact.tsx
+++ src/app/contact-us/components/InstantContact.tsx
@@ -14,7 +14,7 @@ export default function InstantContact({ title, icon, href, body }: ContactProps
       href={href}
       target="_blank"
       rel="noopener noreferrer"
-      className="flex flex-col gap-brand-spacing-sm rounded-brand-radius bg-neutral border border-foreground-borders py-brand-spacing-sm lg:py-brand-spacing lg:px-brand-spacing-lg px-brand-spacing hover:scale-105 duration-200 transition ease-in group hover:bg-accent hover:border-none active:bg-accent active:border-none"
+      className="flex flex-col gap-brand-spacing-sm rounded-brand-radius border border-foreground-borders py-brand-spacing-sm lg:py-brand-spacing lg:px-brand-spacing-lg px-brand-spacing hover:scale-105 duration-200 transition ease-in group hover:bg-accent hover:border-none active:bg-accent active:border-none"
     >
       <h5 className="font-bold">{title}</h5>
       <div className="flex items-center gap-brand-spacing-xs">
